2c531eeca0db4906aaa28e70d867201596eb3f1e
OmarAlKhalaf <99047548@mydavinci.nl>
2022-05-09 15:20:15 +0200

    (done)

---
+++ PizzaCalculatorExapchin.py
@@ -0,0 +1,20 @@
+pizzasmall=6
+pizzamedium=10
+pizzalarge=12.90
+
+def question():
+    global smallpizza,mediumpizza,largepizza
+    smallpizza=int(input("antaal small pizza : "))
+    mediumpizza=int(input("antaal medium pizza : "))
+    largepizza=float(input("antaal large pizza : "))
+
+print('Welcome what piaaz size do you like to have\nThe large one for 12.90$\nThe medium one for 10$\nThe small one for 6$ ? ')
+try:
+    question()
+
+except ValueError :
+    print('I think somthing went wrong pleas try again. ',)
+
+else:
+    print(f'total prijs {largepizza*pizzalarge+mediumpizza*pizzamedium+smallpizza*pizzasmall:.2f}')
+       
